7dbbc342beed9c3ff5bb26f8d94f41cbe3a96212
许亲风 <1161352794@qq.com>
2023-03-03 16:41:35 +0800

    增加数据中台接口等

--- src/data_center.rs
+++ src/data_center.rs
@@ -1,23 +1,26 @@
-use serde::{Serialize,Deserialize};
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize, Serializer};
+use serde::de::DeserializeOwned;
+use crate::data_center::AttrValue::{AttrFloat, AttrStrArray, AttrString};
 
-#[derive(Serialize, Deserialize, Clone, Debug,Default)]
-pub struct DataCenterProject{
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+pub struct DataCenterProject {
     #[serde(rename = "projectCode")]
-    pub project_code:String,
+    pub project_code: String,
     pub owner: String,
     pub instances: Vec<DataCenterInstance>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug,Default)]
-pub struct DataCenterProjectWithRelations{
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+pub struct DataCenterProjectWithRelations {
     #[serde(rename = "projectCode")]
-    pub project_code:String,
+    pub project_code: String,
     pub owner: String,
     pub instances: Vec<DataCenterInstance>,
     pub relations: Vec<DataCenterRelations>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug,Default)]
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct DataCenterInstance {
     #[serde(rename = "objectModelCode")]
     pub object_model_code: String,
@@ -26,7 +29,7 @@ pub struct DataCenterInstance {
     pub attributes: Vec<DataCenterAttr>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug,Default)]
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct DataCenterRelations {
     pub version: String,
     #[serde(rename = "objectModelCode")]
@@ -44,9 +47,36 @@ pub struct DataCenterRelations {
     pub attributes: Vec<u8>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug,Default)]
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct DataCenterAttr {
     #[serde(rename = "attributeModelCode")]
     pub attribute_model_code: String,
-    pub value: String,
+    pub value: AttrValue,
+    // pub value: T,
+}
+
+#[derive(Serialize,Deserialize, Clone, Debug)]
+#[serde(untagged)]
+pub enum AttrValue {
+    AttrString(String),
+    AttrFloat(f32),
+    AttrInt(i32),
+    AttrBool(bool),
+    AttrStrArray(Vec<String>),
+    AttrIntArray(Vec<i32>),
+    AttrMap(HashMap<String,Vec<String>>),
+}
+
+impl Default for AttrValue {
+    fn default() -> Self {
+        AttrString("".to_string())
+    }
+}
+
+#[test]
+fn test_attr_json() {
+    let data = AttrStrArray(vec!["hello".to_string(),"world".to_string()]);
+    let data = AttrFloat(1.0);
+    let json = serde_json::to_string(&data).unwrap();
+    dbg!(&json);
 }
