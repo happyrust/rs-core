b53461a380ac5c89f7b142eaed1d2968f3ce747b
dpc <happydpc@sina.com>
2023-03-24 22:34:53 +0800

    更新模型数据包结构体

--- src/three_dimensional_review.rs
+++ src/three_dimensional_review.rs
@@ -1,15 +1,34 @@
-
 use serde::{Serialize, Deserialize};
+use crate::pdms_types::RefU64;
 
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct ModelDataCrate {
-    pub ProjCode: String,
-    pub UserCode: String,
-    pub SiteCode: String,
-    pub SiteName: String,
-    pub ModelData: Vec<Vec<(String, String)>>,
-    pub FlowData: (String, String),
-    pub ViewData: (String, String),
-    pub KeyValue:String,
+    #[serde(rename="KeyValue")]
+    pub key_value: String,
+    #[serde(rename="ProjCode")]
+    pub proj_code: String,
+    #[serde(rename="UserCode")]
+    pub user_code: String,
+    #[serde(rename="SiteCode")]
+    pub site_code: String,
+    #[serde(rename="SiteName")]
+    pub site_name: String,
+    #[serde(rename="UserRole")]
+    pub user_role: String,
+    #[serde(rename="ModelData")]
+    pub model_data: Vec<Vec<(String, String)>>,
+    #[serde(rename="FlowPicData")] 
+    pub flow_pic_data: Vec<ReviewModelData>,
+
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct ReviewModelData {
+    pub comment: Vec<String>,
+    pub reply: String,
+    pub associatedElement: Vec<RefU64>,
+    pub cloudLine: String,
+    pub viewpoint: String,
+    pub image: String,
 }
