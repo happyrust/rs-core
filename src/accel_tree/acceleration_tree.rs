299d2ee844838c665e9a44300b5faef1c3735d8e
dongpengcheng <golinuxlove@gmail.com>
2023-05-13 00:39:50 +0800

    Update

--- src/accel_tree/acceleration_tree.rs
+++ src/accel_tree/acceleration_tree.rs
@@ -102,7 +102,7 @@ impl AccelerationTree {
         self.tree = rstar::RTree::bulk_load(bounding_boxes);
     }
 
-    pub fn locate_withing_distance<'a>(&'a self, loc: Vec3, distance: f32) -> impl Iterator<Item=RefU64> + 'a {
+    pub fn query_within_distance<'a>(&'a self, loc: Vec3, distance: f32) -> impl Iterator<Item=RefU64> + 'a {
         self.tree
             .locate_within_distance([loc.x, loc.y, loc.z], distance.powi(2))
             .map(|bb| bb.refno)
