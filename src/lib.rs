8790e7548eb3929f393a6e4837b0f27aed628093
dpc <happydpc@sina.com>
2023-06-16 16:37:06 +0800

    ssc配置

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,8 @@ pub mod rvm_types;
 pub mod options;
 pub mod virtual_hole;
 pub mod ssc_setting;
+pub mod penetration;
+pub mod vague_search;
 
 
 // pub type BHashMap<K, V> = bevy::utils::HashMap<K, V>;
---
+++ src/vague_search.rs
@@ -0,0 +1,11 @@
+use bevy::ecs::system::Resource;
+use serde::{Deserialize,Serialize};
+
+#[derive(Resource, Debug, Default, Clone,Deserialize,Serialize)]
+pub struct SearchConditionSave {
+    pub user: String,
+    pub name: String,
+    pub major: String,
+    pub note: String,
+    pub condition: Vec<String>,
+}
