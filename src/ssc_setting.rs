5ae19d5fddd0dcad95ef6fec82e5bec339cc16a5
dpc <happydpc@sina.com>
2023-07-11 10:53:33 +0800

    修改all site获取方式

--- src/ssc_setting.rs
+++ src/ssc_setting.rs
@@ -1,19 +1,20 @@
 use crate::pdms_types::RefU64;
 use bevy_ecs::system::Resource;
-use serde::{Serialize,Deserialize};
-#[derive(Resource,Serialize, Deserialize, Debug, Default, Clone)]
+use serde::{Serialize, Deserialize};
+
+#[derive(Resource, Serialize, Deserialize, PartialEq, Debug, Default, Clone)]
 pub struct SiteData {
     pub refno: RefU64,
     pub name: String,
     pub is_selected: bool,
 }
 
-#[derive(Resource,Serialize, Deserialize, Debug, Default, Clone)]
+#[derive(Resource, Serialize, Deserialize, Debug, Default, Clone)]
 pub struct SiteVec {
     pub data: Vec<SiteData>,
 }
 
-#[derive(Resource,Serialize, Deserialize, Debug, Default, Clone)]
+#[derive(Resource, Serialize, Deserialize, Debug, Default, Clone)]
 pub struct SelectedSiteVec {
     pub data: Vec<SiteData>,
 }
