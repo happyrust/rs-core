203b7870b5954993b8bead46502e8c75c2d64080
dpc <happydpc@sina.com>
2023-08-02 13:31:18 +0800

    更新水工计算

--- src/water_calculation.rs
+++ src/water_calculation.rs
@@ -1,4 +1,6 @@
+use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 use crate::pdms_types::RefU64;
 use bevy_ecs::prelude::Resource;
 use serde::{Deserialize, Serialize};
@@ -40,6 +42,49 @@ pub struct FloodingHoleVec {
 
 #[derive(Component, Clone, Debug, Default, Event, Deserialize, Serialize)]
 pub struct ExportFloodingStpEvent {
+    pub file_name: String,
+    pub save_time: String,
     pub stp: WaterComputeStp,
     pub refnos: Vec<(RefU64, RefU64)>,
+    pub model_list: Vec<(RefU64, String)>,
+    pub all_hole_list: Vec<FloodingHole>,
+    pub selected_hole_list: Vec<FloodingHole>,
+}
+
+impl ExportFloodingStpEvent {
+    pub fn to_arango_struct(self) -> FloodingStpToArangodb {
+        let mut hasher = DefaultHasher::new();
+        self.file_name.hash(&mut hasher);
+        let hash_name = hasher.finish();
+        FloodingStpToArangodb {
+            _key: hash_name.to_string(),
+            save_time: self.save_time,
+            file_name: self.file_name,
+            model_list: self.model_list,
+            all_hole_list: self.all_hole_list,
+            selected_hole_list: self.selected_hole_list,
+        }
+    }
+}
+
+
+#[derive(Component, Clone, Debug, Default, Event, Deserialize, Serialize)]
+pub struct FloodingStpToArangodb {
+    pub _key: String,
+    pub save_time: String,
+    pub file_name: String,
+    pub model_list: Vec<(RefU64, String)>,
+    pub all_hole_list: Vec<FloodingHole>,
+    pub selected_hole_list: Vec<FloodingHole>,
+}
+
+#[derive(Component, Resource, Clone, Debug, Default, Event, Deserialize, Serialize)]
+pub struct FloodingStpToArangodbVec {
+    pub data: Vec<FloodingStpToArangodb>,
+}
+
+#[derive(Component, Clone, Debug, Default, Event, Deserialize, Serialize)]
+pub struct DownloadStpEvent {
+    pub file_name: String,
+    pub contents: Vec<u8>,
 }
