2c0021738540a85d9bf7f53113fff97688d6eb68
dpc <happydpc@sina.com>
2023-09-08 17:23:08 +0800

    fix model problems

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,17 @@ pub mod bin_data;
 
 
 use aios_core::tool::db_tool::{db1_dehash, db1_hash};
+use anyhow::Ok;
 use crate::bin_data::generate_att_info_json;
 
 
 fn main() -> anyhow::Result<()>{
 
     // dbg!(db1_dehash(0x04D852B8));
-    dbg!(db1_dehash(581519));
-    println!("{:#4X}", db1_hash("SUPPO"));
-    generate_att_info_json()
+    // dbg!(db1_dehash(785962));
+    println!("{:#4X}", db1_hash("SPRO"));
+    // generate_att_info_json()
+
+    Ok(())
 }
 
