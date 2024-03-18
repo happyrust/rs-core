59fa254cc6df4a557ac0afe92a37e49dc951d2ba
许亲风 <1161352794@qq.com>
2024-03-18 21:18:32 +0800

    修改材料表单功能

--- src/rs_surreal/material_query.rs
+++ src/rs_surreal/material_query.rs
@@ -247,6 +247,140 @@ impl MaterialTfHavcList {
     }
 }
 
+/// 电气 托盘及接地
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MaterialDqMaterialList {
+    #[serde(deserialize_with = "de_refno_from_key_str")]
+    #[serde(serialize_with = "ser_refno_as_str")]
+    pub id: RefU64,
+    pub num: String,
+    pub project_num: String,
+    pub project_name: Option<String>,
+    pub major: String,
+    pub room_code: String,
+    pub name: String,
+    pub pos: Option<f32>,
+    pub bran_type: Option<String>,
+    pub material: Option<String>,
+    pub width: Option<f32>,
+    pub height: Option<f32>,
+    pub size_num: Option<f32>,
+    pub b_painting: Option<String>,
+    pub painting_color: Option<String>,
+    pub b_cover: Option<String>,
+    pub b_partition: Option<String>,
+    pub partition_num: Option<String>,
+    pub spre: Option<String>,
+    pub catr: Option<String>,
+    pub horizontal_or_vertical: Option<String>,
+    #[serde(default)]
+    pub stander_num: Option<String>,
+    #[serde(default)]
+    pub item_num: Option<String>,
+    #[serde(default)]
+    pub unit: Option<String>,
+    #[serde(default)]
+    pub count: Option<f32>,
+}
+
+
+impl MaterialDqMaterialList {
+    //// 将结构体转为HashMap
+    pub fn into_hashmap(self) -> HashMap<String, String> {
+        let mut map = HashMap::new();
+        map.entry("参考号".to_string()).or_insert(self.id.to_pdms_str());
+        map.entry("机组号".to_string()).or_insert(self.num);
+        map.entry("子项号".to_string()).or_insert(self.project_num);
+        map.entry("子项名称".to_string()).or_insert(self.project_name.unwrap_or("".to_string()));
+        map.entry("专业".to_string()).or_insert(self.major);
+        map.entry("托盘段号".to_string()).or_insert(self.name);
+        map.entry("托盘标高".to_string()).or_insert(self.pos.unwrap_or(0.0).to_string());
+        map.entry("托盘类型".to_string()).or_insert(self.bran_type.unwrap_or("".to_string()));
+        map.entry("材质".to_string()).or_insert(self.material.unwrap_or("".to_string()));
+        map.entry("托盘宽度mm".to_string()).or_insert(self.width.unwrap_or(0.0).to_string());
+        map.entry("托盘高度mm".to_string()).or_insert(self.height.unwrap_or(0.0).to_string());
+        map.entry("规格型号".to_string()).or_insert(self.size_num.unwrap_or(0.0).to_string());
+        map.entry("是否刷漆".to_string()).or_insert(self.b_painting.unwrap_or("".to_string()));
+        map.entry("刷漆颜色".to_string()).or_insert(self.painting_color.unwrap_or("".to_string()));
+        map.entry("有无隔板".to_string()).or_insert(self.b_partition.unwrap_or("".to_string()));
+        map.entry("隔板编号".to_string()).or_insert(self.partition_num.unwrap_or("".to_string()));
+        map.entry("spre".to_string()).or_insert(self.spre.unwrap_or("".to_string()));
+        map.entry("catr".to_string()).or_insert(self.catr.unwrap_or("".to_string()));
+        map.entry("水平/竖向".to_string()).or_insert(self.horizontal_or_vertical.unwrap_or("".to_string()));
+        map.entry("标准号".to_string()).or_insert(self.stander_num.unwrap_or("".to_string()));
+        map.entry("物项编号".to_string()).or_insert(self.item_num.unwrap_or("".to_string()));
+        map.entry("单位".to_string()).or_insert(self.unit.unwrap_or("".to_string()));
+        map.entry("数量".to_string()).or_insert(self.count.unwrap_or(0.0).to_string());
+        map
+    }
+}
+
+/// 电气 托盘及接地
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MaterialDqMaterialListStru {
+    #[serde(deserialize_with = "de_refno_from_key_str")]
+    #[serde(serialize_with = "ser_refno_as_str")]
+    pub id: RefU64,
+    pub num: String,
+    pub project_num: String,
+    pub project_name: String,
+    pub major: String,
+    pub room_code: String,
+    pub pos: Option<f32>,
+    pub material: Option<String>,
+    pub width: Option<f32>,
+    pub height: Option<f32>,
+    pub supp_name: Option<String>,
+    pub size_num: Option<String>,
+    pub b_painting: Option<String>,
+    pub painting_color: Option<String>,
+    pub b_cover: Option<String>,
+    pub b_partition: Option<String>,
+    pub partition_num: Option<String>,
+    pub spre: Option<String>,
+    pub catr: Option<String>,
+    pub horizontal_or_vertical: Option<String>,
+    #[serde(default)]
+    pub stander_num: Option<String>,
+    #[serde(default)]
+    pub item_num: Option<String>,
+    #[serde(default)]
+    pub unit: Option<String>,
+    #[serde(default)]
+    pub count: Option<String>,
+}
+
+
+impl MaterialDqMaterialListStru {
+    //// 将结构体转为HashMap
+    pub fn into_hashmap(self) -> HashMap<String, String> {
+        let mut map = HashMap::new();
+        map.entry("参考号".to_string()).or_insert(self.id.to_pdms_str());
+        map.entry("机组号".to_string()).or_insert(self.num);
+        map.entry("子项号".to_string()).or_insert(self.project_num);
+        map.entry("子项名称".to_string()).or_insert(self.project_name);
+        map.entry("专业".to_string()).or_insert(self.major);
+        map.entry("托盘标高".to_string()).or_insert(self.pos.unwrap_or(0.0).to_string());
+        map.entry("材质".to_string()).or_insert(self.material.unwrap_or("".to_string()));
+        map.entry("托盘支吊架名称".to_string()).or_insert(self.supp_name.unwrap_or("".to_string()));
+        map.entry("托盘宽度mm".to_string()).or_insert(self.width.unwrap_or(0.0).to_string());
+        map.entry("托盘高度mm".to_string()).or_insert(self.height.unwrap_or(0.0).to_string());
+        map.entry("规格型号".to_string()).or_insert(self.size_num.unwrap_or("".to_string()));
+        map.entry("是否刷漆".to_string()).or_insert(self.b_painting.unwrap_or("".to_string()));
+        map.entry("刷漆颜色".to_string()).or_insert(self.painting_color.unwrap_or("".to_string()));
+        map.entry("有无隔板".to_string()).or_insert(self.b_partition.unwrap_or("".to_string()));
+        map.entry("隔板编号".to_string()).or_insert(self.partition_num.unwrap_or("".to_string()));
+        map.entry("spre".to_string()).or_insert(self.spre.unwrap_or("".to_string()));
+        map.entry("catr".to_string()).or_insert(self.catr.unwrap_or("".to_string()));
+        map.entry("水平/竖向".to_string()).or_insert(self.horizontal_or_vertical.unwrap_or("".to_string()));
+        map.entry("标准号".to_string()).or_insert(self.stander_num.unwrap_or("".to_string()));
+        map.entry("物项编号".to_string()).or_insert(self.item_num.unwrap_or("".to_string()));
+        map.entry("单位".to_string()).or_insert(self.unit.unwrap_or("".to_string()));
+        map
+    }
+}
+
+
 ///查询工艺大宗材料数据
 pub async fn get_gy_dzcl(db: Surreal<Any>, refnos: Vec<RefU64>) -> anyhow::Result<(Vec<MaterialGyData>, Vec<MaterialGyDataBend>)> {
     let mut data = Vec::new();
@@ -378,11 +512,113 @@ pub async fn get_gy_equi_list(db: Surreal<Any>, refnos: Vec<RefU64>) -> anyhow::
     Ok(data)
 }
 
-/// 查询电气 托盘及接地
-pub async fn get_dq_bran_list(db: Surreal<Any>, refnos: Vec<RefU64>) -> anyhow::Result<()> {
+/// 查询电气 托盘及接地 托盘
+pub async fn get_dq_bran_list(db: Surreal<Any>, refnos: Vec<RefU64>) -> anyhow::Result<(Vec<MaterialDqMaterialList>, Vec<MaterialDqMaterialListStru>)> {
+    let mut data = Vec::new();
+    let mut stru_data = Vec::new();
+    for refno in refnos {
+        let Some(pe) = get_pe(refno).await? else { continue; };
+        // 如果是site，则需要过滤 site的 name
+        if pe.noun == "SITE".to_string() {
+            if !pe.name.contains("ELEC") { continue; };
+        }
+        // 查询电气托盘的数据
+        let refnos = query_filter_deep_children(refno, vec!["BRAN".to_string()]).await?;
+        let refnos_str = serde_json::to_string(&refnos.into_iter()
+            .map(|refno| refno.to_pe_key()).collect::<Vec<String>>())?;
+        let sql = format!(r#"select
+        id,
+        string::slice(array::at(refno.REFNO->pe_owner.out->pe_owner.out->pe_owner.out.name,0),1,1) as num, // 机组号
+        string::slice(string::split(array::at(refno.REFNO->pe_owner.out->pe_owner.out->pe_owner.out.name,0),'-')[0],2) as project_num, //子项号
+        array::first(refno.REFNO->pe_owner.out->pe_owner.out->pe_owner.out.refno.DESC) as project_name, //子项名称
+        if string::contains(array::at(refno.REFNO->pe_owner.out->pe_owner.out.name,0),'MCT') || string::contains(array::at(refno.REFNO->pe_owner.out->pe_owner.out.name,0),'MSUP')  {{ '主托盘' }} else {{ '次托盘' }} as major,//专业
+        '' as room_code,
+        fn::default_name($this.id) as name,// 托盘段号
+        refno.HPOS[2] as pos, // 托盘标高
+        //<-pe_owner[where in.noun!='ATTA'] order by order_num,
 
+        fn::dq_bran_type($this.id) as bran_type, // 托盘类型
+        '碳钢Q235' as material, // 材质
+        (select value (select value v from only udas.* where u.NAME =='/BranWidth' limit 1) from type::thing("ATT_UDA", meta::id(id)))[0][0] as width, // 托盘宽度
+        (select value (select value v from only udas.* where u.NAME =='/BranHigh' limit 1) from type::thing("ATT_UDA", meta::id(id)))[0][0] as height, // 托盘高度
+        if (select value (select value v from only udas.* where u.NAME =='/BranWidth' limit 1) from type::thing("ATT_UDA", meta::id(id)))[0][0] == NONE {{ 0 }} else {{ (select value (select value v from only udas.* where u.NAME=='/BranWidth' limit 1) from type::thing("ATT_UDA", meta::id(id)))[0][0] }} * if (select value (select value v from only udas.* where u.NAME=='/BranHigh' limit 1) from type::thing("ATT_UDA", meta::id(id)))[0][0] == NONE {{ 0 }} else {{ (select value (select value v from only udas.* where u.NAME=='/BranHigh' limit 1) from type::thing("ATT_UDA", meta::id(id)))[0][0] }} as size_num, // 规格型号
+        'Y' as b_painting, // 是否刷漆
+        string::split(fn::default_name($this.id),'-')[1] as painting_color,
+        if (select value in from only (select * from <-pe_owner[where in.noun != 'ATTA'] order by order_num) limit 1).refno.DESP[2] == 1 {{ '是' }} else {{ '否' }} as b_cover, //有无盖板
+        if refno.DESC == NONE {{ '无' }} else {{ '是' }} as b_partition, // 有无隔板
+        refno.DESC as partition_num, // 隔板编号
+        (select value in from only (select * from <-pe_owner[where in.noun != 'ATTA'] order by order_num) limit 1).refno.SPRE.refno.NAME as spre,
+        (select value in from only (select * from <-pe_owner[where in.noun != 'ATTA'] order by order_num) limit 1).refno.SPRE.refno.CATR.refno.NAME as catr,
+        fn::dq_horizontal_or_vertical($this.id) as horizontal_or_vertical
+        from {}"#, refnos_str);
+        let mut response = db
+            .query(sql)
+            .await?;
+        let mut result: Vec<MaterialDqMaterialList> = response.take(0)?;
+        data.append(&mut result);
+        // 查询电气支吊架的数据
+        let refnos = query_filter_deep_children(refno, vec!["STRU".to_string()]).await?;
+        let refnos_str = serde_json::to_string(&refnos.into_iter()
+            .map(|refno| refno.to_pe_key()).collect::<Vec<String>>())?;
+        let sql = format!(r#"select id,
+        string::slice(array::at(refno.REFNO->pe_owner.out->pe_owner.out.name,0),1,1) as num, // 机组号
+        string::slice(string::split(array::at(refno.REFNO->pe_owner.out->pe_owner.out.name,0),'-')[0],2) as project_num, //子项号
+        array::first(refno.REFNO->pe_owner.out->pe_owner.out.refno.DESC) as project_name, //子项名称
+        '支吊架' as major,//专业
+        '' as room_code, // 房间号 todo
+        fn::default_name($this.id) as supp_name, // 托盘支吊架名称
+        '碳钢Q355' as material,  //材质
+        if (<-pe_owner.in<-pe_owner[where in.noun='SCTN'].in.refno.SPRE.name)[0] == NONE {{ '' }}
+        else {{ array::last(string::split((<-pe_owner.in<-pe_owner[where in.noun='SCTN'].in.refno.SPRE.name)[0],'-')) }} as size_num, // 规格型号
+        (<-pe_owner.in<-pe_owner[where in.noun='SCTN'].in.refno.SPRE.name)[0] as spre,
+        (<-pe_owner.in<-pe_owner[where in.noun='SCTN'].in.refno.SPRE.refno.CATR.refno.NAME)[0] as catr,
+        '2' as count // 数量
+        from {}"#, refnos_str);
+        let mut response = db
+            .query(sql)
+            .await?;
+        let mut result: Vec<MaterialDqMaterialListStru> = response.take(0)?;
+        stru_data.append(&mut result);
+        // 电缆及接地
+        let refnos = query_filter_deep_children(refno, vec!["GENSEC".to_string()]).await?;
+        let refnos_str = serde_json::to_string(&refnos.into_iter()
+            .map(|refno| refno.to_pe_key()).collect::<Vec<String>>())?;
+        let sql = format!(r#"select id,
+        string::slice(array::at(refno.REFNO->pe_owner.out->pe_owner.out.name,0),1,1) as num, // 机组号
+        string::slice(string::split(array::at(refno.REFNO->pe_owner.out->pe_owner.out->pe_owner.out.name,0),'-')[0],2) as project_num, //子项号
+        array::first(refno.REFNO->pe_owner.out->pe_owner.out->pe_owner.out->pe_owner.out.refno.DESC) as project_name, //子项名称
+        '主托盘接地' as major,//专业
+        '' as room_code, // 房间号 todo
+        fn::default_name($this.id) as name, // 托盘段号
+        math::fixed(refno.POS[2],3) as pos,
+        '裸铜缆' as material,  //材质
+        refno.SPRE.refno.NAME as spre,
+        refno.SPRE.refno.CATR.refno.NAME as catr,
+        math::fixed(fn::vec3_distance(array::clump(<-pe_owner.in<-pe_owner[where in.noun='POINSP'].in.refno.POS,3)[0],array::clump(<-pe_owner.in<-pe_owner[where in.noun='POINSP'].in.refno.POS,3)[1]),2) as count
+        from {}"#, refnos_str);
+        let mut response = db
+            .query(sql)
+            .await?;
+        let mut result: Vec<MaterialDqMaterialList> = response.take(0)?;
+        data.append(&mut result);
+    }
+    Ok((data, stru_data))
+}
+
+/// 提前运行定义好的方法
+pub async fn define_surreal_functions(db: Surreal<Any>) -> anyhow::Result<()> {
+    let response = db
+        .query(include_str!("material_list/default_name.surql"))
+        .await?;
+    let response = db
+        .query(include_str!("material_list/dq/fn_dq_bran_type.surql"))
+        .await?;
+    let response = db
+        .query(include_str!("material_list/dq/fn_vec3_distance.surql"))
+        .await?;
     Ok(())
 }
+
 
 #[tokio::test]
 async fn test_get_gy_dzcl() -> anyhow::Result<()> {
