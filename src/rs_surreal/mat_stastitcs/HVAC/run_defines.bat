765bb216b4c7a45b73961af1ebc376892f88144b
happyrust <happydpc@sina.com>
2024-05-10 01:07:39 +0800

    (1) 修改了运行脚本的服务器信息； (2) 简化了函数fn::prev_or_next_is_type，优化了相关函数的实现； (3) 在fln.surql中增加了函数fn::hvac_fl_len_format和fn::hvac_fl_wei_format； (4) 在hvac.surql中增加了函数fn::hvac_len_format； (5) 在STRT.surql中，重构了对STRT的处理方式，创建新的函数fn::collect_strt用来统一处理。

--- src/rs_surreal/mat_stastitcs/HVAC/run_defines.bat
+++ src/rs_surreal/mat_stastitcs/HVAC/run_defines.bat
@@ -1,1 +1,1 @@
-surreal import --conn http://127.0.0.1:8003 --namespace 1516 --database AvevaMarineSample BRCO.surql
+surreal import --conn http://127.0.0.1:8006 --namespace 1516 --database AvevaMarineSample STRT.surql
--- src/rs_surreal/mat_stastitcs/HVAC/tools/run_tool_defines.bat
+++ src/rs_surreal/mat_stastitcs/HVAC/tools/run_tool_defines.bat
@@ -1,9 +1,9 @@
-surreal import --conn http://127.0.0.1:8003 --namespace 1516 --database AvevaMarineSample common.surql
-surreal import --conn http://127.0.0.1:8003 --namespace 1516 --database AvevaMarineSample hvac.surql
-surreal import --conn http://127.0.0.1:8003 --namespace 1516 --database AvevaMarineSample formula.surql
-surreal import --conn http://127.0.0.1:8003 --namespace 1516 --database AvevaMarineSample len.surql
-surreal import --conn http://127.0.0.1:8003 --namespace 1516 --database AvevaMarineSample stif.surql
-surreal import --conn http://127.0.0.1:8003 --namespace 1516 --database AvevaMarineSample fln.surql
-surreal import --conn http://127.0.0.1:8003 --namespace 1516 --database AvevaMarineSample washer.surql
-surreal import --conn http://127.0.0.1:8003 --namespace 1516 --database AvevaMarineSample bolt.surql
+surreal import --conn http://127.0.0.1:8006 --namespace 1516 --database AvevaMarineSample common.surql
+surreal import --conn http://127.0.0.1:8006 --namespace 1516 --database AvevaMarineSample hvac.surql
+surreal import --conn http://127.0.0.1:8006 --namespace 1516 --database AvevaMarineSample formula.surql
+surreal import --conn http://127.0.0.1:8006 --namespace 1516 --database AvevaMarineSample len.surql
+surreal import --conn http://127.0.0.1:8006 --namespace 1516 --database AvevaMarineSample stif.surql
+surreal import --conn http://127.0.0.1:8006 --namespace 1516 --database AvevaMarineSample fln.surql
+surreal import --conn http://127.0.0.1:8006 --namespace 1516 --database AvevaMarineSample washer.surql
+surreal import --conn http://127.0.0.1:8006 --namespace 1516 --database AvevaMarineSample bolt.surql
 
