14149d507b2abb8587e223ba5fd754304fed7c85
happyrust <happydpc@sina.com>
2024-05-19 22:46:35 +0800

    注释掉了版本文件中的打印sql语句以优化代码结构

--- src/rs_surreal/version.rs
+++ src/rs_surreal/version.rs
@@ -4,7 +4,7 @@ use crate::{RefU64, SUL_DB};
 pub async fn backup_att_and_pe_to_history_tables(refnos: &[RefU64]) -> anyhow::Result<()>{
     let pe_keys = refnos.iter().map(|x| x.to_pe_key()).collect::<Vec<_>>().join(",");
     let sql = format!("fn::backup_atts_and_pes([{}])", pe_keys);
-    println!("sql is {}", &sql);
+    // println!("sql is {}", &sql);
     let response = SUL_DB.query(&sql).await.unwrap();
     // dbg!(&response);
     Ok(())
