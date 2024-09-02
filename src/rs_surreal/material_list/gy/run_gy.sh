133db58caa52470144021ffc1f9d726d7560f99b
dongpengcheng <happydpc@sina.com>
2024-09-02 10:09:01 +0800

    add missing surql

---
+++ src/rs_surreal/material_list/gy/run_gy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#将 http 地址 取自环境变量
+HTTP_ADDR=${HTTP_ADDR:-http://127.0.0.1:8008}
+#database name
+DATABASE_NAME=${DATABASE_NAME:-AvevaMarineSample}
+#namespace
+NAMESPACE=${NAMESPACE:-1516}
+
+surql_files=(
+    "gy_common.surql"
+    "gy_bend.surql"
+    "gy_part.surql"
+    "gy_tubi.surql"
+    "gy_valve.surql"
+    "gy_collect.surql"
+)
+
+for file in "${surql_files[@]}"; do
+    surreal import --conn $HTTP_ADDR --namespace $NAMESPACE --database $DATABASE_NAME -u root -p root $file
+done
