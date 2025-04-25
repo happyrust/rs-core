8b61f47fb41a9460ce1d0a5ab67aecb9ae372a1f
happyrust <happydpc@sina.com>
2025-04-25 17:44:42 +0800

    (1) 删除了`mod.rs`文件中的多余空行，优化代码结构。 (2) 新增了`test_different_dbs`模块，用于测试不同数据库的支持。 (3) 升级了`dashmap`依赖版本至`7.0.0-rc2`，并保留`serde`特性。 (4) 增加了对`duckdb`库的依赖，版本为`1.2.2`。

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -6,7 +6,6 @@ pub mod test_wire;
 
 pub mod test_schema;
 
-
 pub mod test_refno;
 
 pub mod test_rotation;
@@ -17,12 +16,13 @@ pub mod test_serde;
 
 pub mod test_spatial_calculation;
 
-
 pub mod test_parse_dir;
 
 #[cfg(not(target_arch = "wasm32"))]
 pub mod test_surreal;
 
 pub mod test_expression;
+
+pub mod test_different_dbs;
 
 // pub mod test_material;
